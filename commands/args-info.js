e81ca2e674d65ac4b9c042ca0432d7fcbfe19f9a
Mathew Antich <mifu@allfearthesentinel.net>
2020-07-25 23:17:40 +0800

    Changed Usage notes on command

--- commands/args-info.js
+++ commands/args-info.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'args-info',
     description: 'Information about the arguments provided. This is just some stupid learning command. Will dissappear later',
     args: true,
-    usage: '<arg1>, <arg2> etc',
+    usage: '<arg1> <arg2> <etc>',
 	execute(message, args) {
 		if (args[0] === 'foo') {
 			return message.channel.send('bar');
