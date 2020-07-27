f5884a8bf1b5d4c07b88088188eb0d2deae33372
Mathew Antich <mifu@allfearthesentinel.net>
2020-07-27 20:10:23 +0800

    Added new function trimArray()

--- Structures/Util.js
+++ Structures/Util.js
@@ -51,4 +51,14 @@ module.exports = class Util {
             }
         });
     }
+
+    trimArray(arr, maxLen = 10) {
+        if (arr.length > maxLen) {
+            const len = arr.length - maxLen;
+            arr = arr.slice(0, maxLen);
+            arr.push(`${len} more...`);
+        }
+
+        return arr;
+    }
 };
