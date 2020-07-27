249e0a29a8350dfadcce1f1f8612816b4a771870
Mathew Antich <mifu@allfearthesentinel.net>
2020-07-27 21:17:35 +0800

    Added another module (moment)

---
+++ commands/Utilities/ServerInfo.js
@@ -0,0 +1,49 @@
+const Command = require('../../Structures/Command');
+const { MessageEmbed } = require('discord.js');
+const moment = require('moment');
+
+const filterLevels = {
+	DISABLED: 'Off',
+	MEMBERS_WITHOUT_ROLES: 'No Role',
+	ALL_MEMBERS: 'Everyone'
+};
+
+const verificationLevels = {
+	NONE: 'None',
+	LOW: 'Low',
+	MEDIUM: 'Medium',
+	HIGH: '(╯°□°）╯︵ ┻━┻',
+	VERY_HIGH: '┻━┻ ﾐヽ(ಠ益ಠ)ノ彡┻━┻'
+};
+
+const regions = {
+	brazil: 'Brazil',
+	europe: 'Europe',
+	hongkong: 'Hong Kong',
+	india: 'India',
+	japan: 'Japan',
+	russia: 'Russia',
+	singapore: 'Singapore',
+	southafrica: 'South Africa',
+	sydeny: 'Sydeny',
+	'us-central': 'US Central',
+	'us-east': 'US East',
+	'us-west': 'US West',
+	'us-south': 'US South'
+};
+
+module.exports = class extends Command {
+
+    constructor(...args) {
+        super(...args, {
+            name: 'serverinfo',
+            description: 'A debug command to check latency of the bot',
+            aliases: ['pong', 'latency'],
+        });
+    }
+
+    // eslint-disable-next-line no-unused-vars
+    async run(message) {
+        
+    }
+};
