1c1399a80d1ae6ed03ed4c29e053924d2104cf71
Mathew Antich <mifu@allfearthesentinel.net>
2020-07-30 18:35:24 +0800

    Created a Even handler with two new functions

---
+++ Structures/Event.js
@@ -0,0 +1,27 @@
+/*
+    ---------------------------------------------------------------------
+    ---------------------------------------------------------------------
+    ----------              Mythotic Tinker bot                ----------
+    ---------------------------------------------------------------------
+    ---------------------------------------------------------------------
+
+    Event.js
+
+    This file the base class for Event. Each Event should be extending
+    this class in order for it to work.
+*/
+
+module.exports = class Event {
+
+    constructor(client, name, options = {}) {
+        this.name = name;
+        this.client = client;
+        this.type = options.once ? 'once' : 'on';
+        this.emitter = (typeof options.emitter === 'string' ? this.client[options.emitter] : 'options.emitter') || this.client;
+    }
+
+    // eslint-disable-next-line no-unused-vars
+    async run(...args) {
+        throw new Error(`The run method has not been implemented in ${this.name}`);
+    }
+};
--- Structures/Util.js
+++ Structures/Util.js
@@ -15,6 +15,7 @@ const path = require('path');
 const { promisify } = require('util');
 const glob = promisify(require('glob'));
 const Command = require('./Command.js');
+const Event = require('./Event');
 
 module.exports = class Util {
 
@@ -63,6 +64,21 @@ module.exports = class Util {
                     }
                 }
 
+            }
+        });
+    }
+
+    async loadEvents() {
+        return glob(`${this.directory}events/**/*.js`).then(events => {
+            for (const eventFile of events) {
+                delete require.cache(eventFile);
+                const { name } = path.parse(eventFile);
+                const File = require(eventFile);
+                if (!this.isClass(File)) throw new TypeError(`Event ${name} does not seem to be a Discord Event or does not export a class.`);
+                const event = new File(this.client, name.toLowerCase());
+                if (!(event instanceof Event)) throw new TypeError(`Event ${name} does not belong in Events`);
+                this.client.events.set(event.name, event);
+                event.emitter[event.type](name, (...args) => event.run(...args));
             }
         });
     }
