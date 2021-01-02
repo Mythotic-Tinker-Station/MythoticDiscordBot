ffc44a77a135fadac6d1ecf961bd7cbc7282d2f1
Mathew Antich <mifu@allfearthesentinel.net>
2021-01-02 15:03:39 +0800

    Updated for loop for presenceUpdates

--- Events/presence/presenceUpdate.ts
+++ Events/presence/presenceUpdate.ts
@@ -14,13 +14,15 @@ module.exports = class extends (Event) {
 
     async run(oldPresence, newPresence) {
         // Handle each presence type here when required. Obviously streaming first :)
+        console.log(newPresence)
         if (!newPresence.activities) return false;
-        newPresence.activities.forEach(activity => {
+        for (const activity of newPresence.activities) {
             if (activity.type == "STREAMING") {
                 console.log(`${newPresence.user.tag} is streaming at ${activity.url}.`);
 
                 // We need to determine if the user is in a server where they have the streaming settings configured
+                
             };
-        })
+        }
     }
 }
