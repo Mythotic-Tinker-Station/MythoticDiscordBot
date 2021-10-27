ee1de5409bd5e27318a374e3d81ce627dae7039b
Mathew Antich <mifu@allfearthesentinel.net>
2021-10-27 23:24:18 +0800

    Restructured the bot client into its own file and added commands

---
+++ Bot.cs
@@ -0,0 +1,74 @@
+﻿/* 
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ~ The Mythotic Discord Bot ~
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Main file - Contains the initalization for the bot
+
+
+ */
+
+using DSharpPlus;
+using System;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+using static MythoticDiscordBot.JsonClasses;
+using MythoticDiscordBot;
+using DSharpPlus.EventArgs;
+using DSharpPlus.Interactivity.Extensions;
+using DSharpPlus.Interactivity;
+using DSharpPlus.CommandsNext;
+using MythoticDiscordBot.Commands;
+
+namespace MythoticDiscordBot.Bot
+{
+    class BotClient
+    {
+        static EventLogic events;
+        public DiscordClient discord { get; private set; }
+        public InteractivityExtension Interactivity { get; private set; }
+        public CommandsNextExtension Commands { get; private set; }
+
+        public async Task RunAsync()
+        {
+            // First, lets read the config!
+            ConfigJson config = JsonSerializer.Deserialize<ConfigJson>(File.ReadAllText("Config\\config.json"));
+
+            // Setup the Discord Client
+            DiscordConfiguration DiscordConfig = new(new()
+            {
+                Token = config.Token,
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.All
+            });
+
+            DiscordClient discord = new(DiscordConfig);
+
+            discord.UseInteractivity(new InteractivityConfiguration
+            {
+                Timeout = TimeSpan.FromMinutes(2)
+            });
+
+            CommandsNextConfiguration commandsConfig = new CommandsNextConfiguration
+            {
+                StringPrefixes = new string[] { "!" },
+                EnableDms = false,
+                EnableMentionPrefix = true,
+                DmHelp = true,
+            };
+
+            Commands = discord.UseCommandsNext(commandsConfig);
+
+            Commands.RegisterCommands<UtilityCommands>();
+
+            //CommandLogic.SetCommand("test", "Testing!");
+            events = new(discord);
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,45 +1,13 @@
-﻿/* 
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    ~ The Mythotic Discord Bot ~
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+﻿using MythoticDiscordBot.Bot;
 
-    Main file - Contains the initalization for the bot
-
-
- */
-
-using DSharpPlus;
-using System;
-using System.Threading.Tasks;
-using System.IO;
-using System.Text.Json;
-using static MythoticDiscordBot.JsonClasses;
-using MythoticDiscordBot;
-
-namespace MythoticBot
+namespace MythoticDiscordBot
 {
     class Program
     {
-        static EventLogic events;
-
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
-            // First, lets read the config!
-            ConfigJson config = JsonSerializer.Deserialize<ConfigJson>(File.ReadAllText("Config\\config.json"));
-
-            // Setup the Discord Client
-            DiscordClient discord = new(new()
-            {
-                Token = config.Token,
-                TokenType = TokenType.Bot,
-                Intents = DiscordIntents.All
-            });
-
-            CommandLogic.SetCommand("test", "Testing!");
-            events = new(discord);
-
-            await discord.ConnectAsync();
-            await Task.Delay(-1);
+            BotClient bot = new BotClient();
+            bot.RunAsync().GetAwaiter().GetResult();
         }
     }
 }
