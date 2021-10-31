3a504cded30e96d7340cc546948fb05ee18b4a6f
Mathew Antich <mifu@allfearthesentinel.net>
2021-10-31 10:02:34 +0800

    Changed response command  to RespondAsync()

--- Commands/UtilityCommands.cs
+++ Commands/UtilityCommands.cs
@@ -17,7 +17,7 @@ namespace MythoticDiscordBot.Commands
         [Description("It returns pong... How orginial")]
         public async Task Ping(CommandContext ctx)
         {
-            await ctx.Channel.SendMessageAsync("Pong").ConfigureAwait(false);
+            await ctx.RespondAsync("Pong").ConfigureAwait(false);
         }
     }
 }
