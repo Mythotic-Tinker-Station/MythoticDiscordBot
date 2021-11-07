49d22d7cd2477a7a2b6c6aadfc9a393cdf52771d
Mathew Antich <mifu@allfearthesentinel.net>
2021-11-07 22:11:08 +0800

    Added Serverinfo Command

--- Commands/UtilityCommands.cs
+++ Commands/UtilityCommands.cs
@@ -31,5 +31,41 @@ namespace MythoticDiscordBot.Commands
         {
             await ctx.RespondAsync("Pong").ConfigureAwait(false);
         }
+
+        // Display Discord Server info
+        [Command("serverinfo")]
+        [Description("Displays the Server information your currently in")]
+        [Aliases("servinfo", "serverinf")]
+        public async Task ServerInfo(CommandContext ctx)
+        {
+            string boostTier = ctx.Guild.PremiumTier.ToString();
+
+            DiscordEmbed discordEmbed = new DiscordEmbedBuilder()
+                .WithTitle(ctx.Guild.Name)
+                .WithAuthor(ctx.Message.Author.Username)
+                .WithColor(DiscordColor.Cyan)
+                .WithDescription("Server Information")
+                .AddField("General", $"**❯ Name:** {ctx.Guild.Name}\n" +
+                $"**❯ ID:** {ctx.Guild.Id}\n" +
+                $"**❯ Owner:** {ctx.Guild.Owner.Username} ({ctx.Guild.OwnerId})\n" +
+                $"**❯ Boost Tier:** {boostTier}\n" +
+                $"**❯ Explicit Filter:** {ctx.Guild.ExplicitContentFilter}\n" +
+                $"**❯ Verification Level:** {ctx.Guild.VerificationLevel}\n" +
+                $"**❯ Time Created:** {ctx.Guild.CreationTimestamp.DateTime}\n")
+
+                .AddField("Stats", $"**❯ Role Count:** {ctx.Guild.Roles.Count}\n" +
+                $"**❯ Emoji Count:** {ctx.Guild.Emojis.Count}\n" +
+                $"**❯ Members:** {ctx.Guild.Members.Count}\n" +
+                $"**❯ Channels:** {ctx.Guild.Channels.Count}\n" +
+                $"**❯ Boost Count:** {ctx.Guild.PremiumSubscriptionCount}\n")
+
+                // Add Presence stats later, dont know what prop has them
+                .Build();
+
+            DiscordMessage message = await new DiscordMessageBuilder()
+                .WithEmbed(discordEmbed)
+                .SendAsync(ctx.Channel);
+        }
     }
+
 }
