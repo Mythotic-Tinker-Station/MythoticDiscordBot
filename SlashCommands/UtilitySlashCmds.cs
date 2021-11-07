739bb85ea739450df4f21ebe4a0ac8d82298f455
Mathew Antich <mifu@allfearthesentinel.net>
2021-11-07 22:11:18 +0800

    Added ping slash command

---
+++ SlashCommands/UtilitySlashCmds.cs
@@ -0,0 +1,37 @@
+﻿/* 
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ~ The Mythotic Discord Bot ~
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    UtilitySlashCmds.cs - Utility Slash Command Module
+
+    Contains all commands that are utility such as ping, display server info etc. This should be an exact mirror of the Commands Class
+
+
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+using DSharpPlus.SlashCommands.Attributes;
+
+namespace MythoticDiscordBot.SlashCommands
+{
+    public class UtilitySlashCmds : ApplicationCommandModule
+    {
+        // Just a Ping Command
+        [SlashCommand("ping", "A Test command")]
+        public async Task Ping(InteractionContext ctx)
+        {
+            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent("Pong"));
+        }
+    }
+}
