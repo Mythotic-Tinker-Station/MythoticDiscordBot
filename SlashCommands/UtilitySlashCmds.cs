b202a1ce4bac3441796ce99ce576d011f56fab45
Mathew Antich <mifu@allfearthesentinel.net>
2021-11-11 21:15:26 +0800

    Added serverinfo slash command

--- SlashCommands/UtilitySlashCmds.cs
+++ SlashCommands/UtilitySlashCmds.cs
@@ -33,5 +33,37 @@ namespace MythoticDiscordBot.SlashCommands
         {
             await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent("Pong"));
         }
+
+        // Display Discord Server info
+        [SlashCommand("serverinfo", "Displays the Server information your currently in")]
+        public async Task ServerInfo(InteractionContext ctx)
+        {
+            string boostTier = ctx.Guild.PremiumTier.ToString();
+
+            DiscordEmbed discordEmbed = new DiscordEmbedBuilder()
+                .WithTitle(ctx.Guild.Name)
+                .WithAuthor(ctx.Member.Username)
+                .WithColor(DiscordColor.Cyan)
+                .WithImageUrl(ctx.Guild.IconUrl)
+                .WithDescription("Server Information")
+                .AddField("General", $"**❯ Name:** {ctx.Guild.Name}\n" +
+                $"**❯ ID:** {ctx.Guild.Id}\n" +
+                $"**❯ Owner:** {ctx.Guild.Owner.Username} ({ctx.Guild.OwnerId})\n" +
+                $"**❯ Boost Tier:** {boostTier}\n" +
+                $"**❯ Explicit Filter:** {ctx.Guild.ExplicitContentFilter}\n" +
+                $"**❯ Verification Level:** {ctx.Guild.VerificationLevel}\n" +
+                $"**❯ Time Created:** {ctx.Guild.CreationTimestamp.DateTime}\n")
+
+                .AddField("Stats", $"**❯ Role Count:** {ctx.Guild.Roles.Count}\n" +
+                $"**❯ Emoji Count:** {ctx.Guild.Emojis.Count}\n" +
+                $"**❯ Members:** {ctx.Guild.Members.Count}\n" +
+                $"**❯ Channels:** {ctx.Guild.Channels.Count}\n" +
+                $"**❯ Boost Count:** {ctx.Guild.PremiumSubscriptionCount}\n")
+
+                // Add Presence stats later, dont know what prop has them
+                .Build();
+
+            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().AddEmbed(discordEmbed));
+        }
     }
 }
