4291631f8dbd220a443f6a6dc51e260994b519ed
Mathew Antich <mifu@allfearthesentinel.net>
2021-11-17 23:11:07 +0800

    Created ServerConfig Model

---
+++ MythoticDiscordBot.DAL/Models/ServerConfig/ServerConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MythoticDiscordBot.DAL.Models.ServerConfig
+{
+    public class ServerConfig : Entity
+    {
+        public string ServerId { get; set; }
+        public string ServerName { get; set; }
+        public string[]? AdminRoles { get; set; }
+        public string[]? ModeratorRoles { get; set; }
+        public string Prefix { get; set; }
+    }
+}
