832ac24deefe082799e2578d6bb0966012826d61
Mathew Antich <mifu@allfearthesentinel.net>
2021-11-18 21:15:58 +0800

    Created ServerConfig DB Context

---
+++ MythoticDiscordBot.DAL/ServerConfigContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MythoticDiscordBot.DAL.Models.ServerConfig;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MythoticDiscordBot.DAL
+{
+    public class ServerConfigContext : DbContext
+    {
+        public ServerConfigContext(DbContextOptions<ServerConfigContext> options) : base(options) { }
+
+        public DbSet<ServerConfig> ServerConfigs { get; set; }
+    }
+}
