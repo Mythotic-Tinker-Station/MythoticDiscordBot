f3051b45de4f3e4175874a99a7cd7ff0b5ca188a
Mathew Antich <mifu@allfearthesentinel.net>
2021-11-18 21:32:38 +0800

    Added Aditional code for Program.cs

--- MythoticDiscordBot.Bot/Program.cs
+++ MythoticDiscordBot.Bot/Program.cs
@@ -1,15 +1,24 @@
-﻿using MythoticDiscordBot.Bot;
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Hosting;
+using MythoticDiscordBot.Bot;
 
 namespace MythoticDiscordBot.Bot
 {
     class Program
     {
         public static DateTime ReadyTime { get; set; }
-
+        
         static void Main(string[] args)
         {
             BotClient bot = new BotClient();
             bot.RunAsync().GetAwaiter().GetResult();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
     }
 }
