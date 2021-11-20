bb151908fea70af19792dab51b24ffe5a0d6482d
Mathew Antich <mifu@allfearthesentinel.net>
2021-11-20 12:56:08 +0800

    Updated packages for migrations

---
+++ MythoticDiscordBot.DAL.Migrations/Migrations/20211120042307_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MythoticDiscordBot.DAL.Migrations.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ServerConfigs",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ServerId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ServerName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AdminRole = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ModeratorRole = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Prefix = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ServerConfigs", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ServerConfigs");
+        }
+    }
+}
