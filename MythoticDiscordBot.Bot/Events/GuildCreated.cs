f46adb446bdcf646a0088e6371029e3d0f8ce26a
Mathew Antich <mifu@allfearthesentinel.net>
2021-11-21 16:33:35 +0800

    Created GuildCreate Event, adds info to database when bot joins

---
+++ MythoticDiscordBot.Bot/Events/GuildCreated.cs
@@ -0,0 +1,44 @@
+﻿using DSharpPlus;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MythoticDiscordBot.DAL.Models.ServerConfig;
+using MythoticDiscordBot.Core.Services.ServerConfigService;
+
+namespace MythoticDiscordBot.Bot.Events
+{
+    public class GuildCreated
+    {
+        private readonly IServerConfigService _serverConfigService;
+
+        public GuildCreated(IServerConfigService serverConfigService)
+        {
+            _serverConfigService = serverConfigService;
+        }
+        
+        public async Task Discord_GuildCreated(DiscordClient client, DSharpPlus.EventArgs.GuildCreateEventArgs guild)
+        {
+            // First, lets build an ServerConfig
+            ServerConfig config = new ServerConfig
+            {
+                ServerId = guild.Guild.Id,
+                ServerName = guild.Guild.Name,
+                Prefix = "!"
+            };
+
+            try
+            {
+                await _serverConfigService.CreateServerConfig(config);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex);
+            }
+            
+        }
+    }
+}
