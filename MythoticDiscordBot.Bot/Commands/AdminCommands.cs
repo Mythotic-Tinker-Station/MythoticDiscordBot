741684a0c16157b961ba1783a0d0dc0062ad96ca
Mathew Antich <mifu@allfearthesentinel.net>
2022-01-31 08:33:00 +0800

    Started admincommands class. for setting change etc

---
+++ MythoticDiscordBot.Bot/Commands/AdminCommands.cs
@@ -0,0 +1,31 @@
+﻿using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Entities;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MythoticDiscordBot.Bot.Interfaces;
+using MythoticDiscordBot.DAL;
+
+namespace MythoticDiscordBot.Bot.Commands
+{
+    public class AdminCommands : BaseCommandModule, ICommandCategory
+    {
+        public string Category()
+        {
+            return "Administration";
+        }
+
+        private readonly ServerConfigContext _context;
+
+        public AdminCommands(ServerConfigContext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}
