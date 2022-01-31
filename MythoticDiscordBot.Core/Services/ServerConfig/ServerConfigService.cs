a7c7b7bc62a570321556c51d3abf296ae812e09d
Mathew Antich <mifu@allfearthesentinel.net>
2022-01-31 08:32:38 +0800

    Added few more database commands

--- MythoticDiscordBot.Core/Services/ServerConfig/ServerConfigService.cs
+++ MythoticDiscordBot.Core/Services/ServerConfig/ServerConfigService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using MythoticDiscordBot.DAL;
 using MythoticDiscordBot.DAL.Models.ServerConfig;
 
@@ -11,9 +13,10 @@ namespace MythoticDiscordBot.Core.Services.ServerConfigService
     public interface IServerConfigService
     {
         ServerConfigContext Context { get; }
-
         Task<ServerConfig> GetServerConfigByServerId(string serverId);
         Task CreateServerConfig(ServerConfig config);
+        Task UpdateServerConfig_ServerName(ServerConfig config, string value);
+        //Task DeleteServerConfig(ServerConfig config);
     }
 
     public class ServerConfigService : IServerConfigService
@@ -35,7 +38,26 @@ namespace MythoticDiscordBot.Core.Services.ServerConfigService
 
         public async Task<ServerConfig> GetServerConfigByServerId(string serverId)
         {
-            throw new NotImplementedException();
+            ulong newserverId = Convert.ToUInt64(serverId);
+            return await _context.ServerConfigs.FirstOrDefaultAsync(x => x.ServerId == newserverId);
+
+        }
+
+        public async Task UpdateServerConfig_ServerName(ServerConfig config, string value)
+        {
+            ulong serverId = config.ServerId;
+            ServerConfig Config = await _context.ServerConfigs.Where(x => x.ServerId == serverId).FirstOrDefaultAsync();
+
+            if (Config == null)
+            {
+                throw new Exception("CRITICAL: Server config is not in the database");
+            }
+            else
+            {
+                Config.ServerName = value;
+                await _context.SaveChangesAsync();
+            }
+            
         }
     }
 }
